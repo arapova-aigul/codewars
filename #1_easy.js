c71c460982914da9752333a70e8f9b35474ad876
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 14:58:04 +0500

    feat: add solution "String ends with?"

--- #1_easy.js
+++ #1_easy.js
@@ -152,3 +152,14 @@ function removeChar(str) {
 function noSpace(x) {
     return x.split(" ").join("");
 }
+
+/*
+    Name: String ends with?
+    Description: Complete the solution so that it returns true if the first argument(string) 
+    passed in ends with the 2nd argument (also a string).
+*/
+
+function solution(str, ending) {
+    const index = str.length - ending.length;
+    return str.includes(ending,index);
+}
