6b0d196b76b9d0ed27891a3e9c9cf4bbfc699a3e
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 14:10:13 +0500

    feat: add solution "Can we divide it?"

--- #1_easy.js
+++ #1_easy.js
@@ -4,5 +4,15 @@
 */
 
 function greet() {
-    return "hello world!"
+    return "hello world!";
+}
+
+/*
+    Name: Can we divide it?
+    Description: Your task is to create the functionisDivideBy (or is_divide_by) to check if an 
+    integer number is divisible by both integers a and b.
+*/
+
+function isDivideBy(a,b,c) {
+    return (a % b == 0) && (a % c == 0);
 }
