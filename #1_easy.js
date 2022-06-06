3d63f6e1222d5aad5f80e3c7b70dcd6f293b3d38
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 14:40:11 +0500

    feat: add solution "Largest Square Inside A Circle"

--- #1_easy.js
+++ #1_easy.js
@@ -94,3 +94,12 @@ function fakeBin(x) {
     let result = resStr.split(',').join(''); 
     return result;
 }
+
+/*
+    Name: Largest Square Inside A Circle
+    Description: Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+
+function areaLargestSquare(r) {
+    return r * r * 2;
+}
