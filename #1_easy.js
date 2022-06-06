c329aba8d582f209a1a492fd42f9d32af37cb1b8
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 14:59:50 +0500

    feat: add solution "Simple multiplication"

--- #1_easy.js
+++ #1_easy.js
@@ -163,3 +163,13 @@ function solution(str, ending) {
     const index = str.length - ending.length;
     return str.includes(ending,index);
 }
+
+/*
+    Name: Simple multiplication
+    Description: This kata is about multiplying a given number by eight if it is an even number 
+    and by nine otherwise.
+*/
+
+function simpleMultiplication(number) {
+    return (number % 2 == 0) ? number * 8 : number * 9;
+}
