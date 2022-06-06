42f0ae06c9ed2c20f4207cf1639d3e4b58f40a93
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 21:36:14 +0500

    feat: add solution "16+18=214"

--- #2_intermediate.js
+++ #2_intermediate.js
@@ -31,3 +31,26 @@ function mergeArrays(arr1, arr2) {
     arr3 = Array.from(new Set(arr3)); // remove duplicated
     return arr3.sort(function(a, b) { return a - b; }); // sorted
 }
+
+/*
+    Name: 16+18=214
+    Description: For this kata you will have to forget how to add two numbers. In simple terms, our method does 
+    not like the principle of carrying over numbers and just writes down every number it calculates.
+    You may assume both integers are positive integers.
+*/
+
+function add(num1, num2) {
+    const arrNum1 = Array.from(String(num1.toString()), Number); 
+    const arrNum2 = Array.from(String(num2.toString()), Number); 
+    let result = '';
+    
+    while (arrNum1.length > arrNum2.length) arrNum2.unshift(0);  
+    while (arrNum1.length < arrNum2.length) arrNum1.unshift(0);    
+    
+    if (arrNum2.length == arrNum1.length) {
+      arrNum2.forEach((element,index) => {
+        result = result + Number(element + arrNum1[index]);
+      });
+    }
+    return Number(result);
+}
