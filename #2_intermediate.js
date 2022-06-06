b49166384cb3d8342b73ad501d0427c4b20b25be
Arapova Aigul <arapova.aygul@yandex.ru>
2022-06-06 21:19:12 +0500

    feat: add solution "Merge two sorted arrays into one"

--- #2_intermediate.js
+++ #2_intermediate.js
@@ -11,3 +11,23 @@
 var ArrowFunc = function(arr) {
     return arr.map(x => String.fromCharCode(x)).join(''); 
 };
+
+/*
+    Name: Merge two sorted arrays into one
+    Description: You are given two sorted arrays that both only contain integers. 
+    Your task is to find a way to merge them into a single one, sorted in asc order. 
+    Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+    You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. 
+    If both arr1 and arr2 are empty, then just return an empty array.
+    Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. 
+    Remove duplicated in the returned result.
+*/
+
+function mergeArrays(arr1, arr2) {
+    let arr3 = arr1.slice();
+    for (let key in arr2) {
+      arr3.push(arr2[key]);
+    }
+    arr3 = Array.from(new Set(arr3)); // remove duplicated
+    return arr3.sort(function(a, b) { return a - b; }); // sorted
+}
